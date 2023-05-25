b4f80b26616e94e1e330d72d97eb920b7103a475
Julian Kimmig <julian.kimmig@gmx.net>
2023-05-25 17:29:50 +0200

    importing

--- funcnodes/__init__.py
+++ funcnodes/__init__.py
@@ -1,1 +1,22 @@
 __version__ = "0.1.0"
+
+from funcnodes.nodespace import NodeSpace
+from funcnodes.node import Node
+from funcnodes.io import NodeInput, NodeOutput, NodeIO, Edge
+from funcnodes.nodes.node_creator import (
+    func_to_node,
+    func_to_node_decorator,
+    NodeClassMixin,
+)
+
+__all__ = [
+    "NodeSpace",
+    "Node",
+    "NodeInput",
+    "NodeOutput",
+    "NodeIO",
+    "Edge",
+    "func_to_node",
+    "func_to_node_decorator",
+    "NodeClassMixin",
+]
---
+++ funcnodes/worker/__init__.py
@@ -0,0 +1,6 @@
+from funcnodes.worker.worker import Worker, RemoteWorker
+from funcnodes.worker.external_worker import FuncNodesExternalWorker
+from funcnodes.worker.loop import CustomLoop
+
+
+__all__ = ["Worker", "RemoteWorker", "FuncNodesExternalWorker", "CustomLoop"]
