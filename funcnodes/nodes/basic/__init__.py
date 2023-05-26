8c7cc2d329edc563cb4995b5bf252fb20810b7d8
Julian Kimmig <julian.kimmig@gmx.net>
2023-05-26 17:01:15 +0200

    textnodes

--- funcnodes/nodes/basic/__init__.py
+++ funcnodes/nodes/basic/__init__.py
@@ -2,10 +2,11 @@ from ...nodespace import LibShelf
 from ...node import Node
 from ...io import NodeInput, NodeOutput
 from .inputnodes import LIB as INPUT_LIB
+from .textnodes import LIB as TEXT_LIB
 
 
 LIB = LibShelf(
     name="basic",
     nodes=[],
-    shelves=[INPUT_LIB],
+    shelves=[INPUT_LIB,TEXT_LIB],
 )
---
+++ funcnodes/nodes/basic/textnodes.py
@@ -0,0 +1,50 @@
+from funcnodes import Node, NodeInput, NodeOutput
+from ...nodespace import LibShelf
+
+
+class TextSplitNode(Node):
+    node_id = "text.split"
+    text = NodeInput(type=str, required=True)
+    sep = NodeInput(type=str, required=True)
+    before = NodeOutput(type=str)
+    after = NodeOutput(type=str)
+
+    async def on_trigger(self):
+        text = self.text.value
+        sep = self.sep.value
+        before, after = text.split(sep, 1)
+        self.before.value = before
+        self.after.value = after
+        return True
+
+
+class ToKeyValuePair(Node):
+    node_id = "text.kv_pair"
+    text = NodeInput(type=str, required=True)
+    sep = NodeInput(type=str, required=True)
+    output = NodeOutput(type=dict)
+
+    async def on_trigger(self):
+        text = self.text.value
+        sep= self.sep.value
+        sep = bytes(sep, "utf-8").decode("unicode_escape")
+        if not sep:
+            return False
+
+        out = {}
+        for line in text.split("\n"):
+            if sep not in line:
+                continue
+            key, value = line.split(sep, 1)
+            out[key.strip()] = value.strip()
+        self.output.value = out
+        return True
+
+
+LIB = LibShelf(
+    name="text",
+    nodes=[
+        TextSplitNode,
+    ],
+    shelves=[],
+)
