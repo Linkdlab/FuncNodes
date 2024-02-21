aa1fadc38297421315ae3fbef9c46501b724fa81
Julian Kimmig <julian.kimmig@gmx.net>
2024-02-21 14:49:01 +0100

    nodes

--- funcnodes/basic_nodes/logic.py
+++ funcnodes/basic_nodes/logic.py
@@ -1,10 +1,12 @@
 from funcnodes.node import Node, TriggerStack
-from typing import Any
-from funcnodes.io import NodeInput, NodeOutput
+from typing import Any, List
+from funcnodes.io import NodeInput, NodeOutput, NoValue
+import asyncio
 
 
 class IfNode(Node):
     node_id = "if_node"
+    node_name = "If"
     on_true = NodeOutput(id="on_true", type=Any)
     on_false = NodeOutput(id="on_false", type=Any)
     condition = NodeInput(id="condition", type=bool)
@@ -19,6 +21,7 @@ class IfNode(Node):
 
 class WhileNode(Node):
     node_id = "while_node"
+    node_name = "While"
     condition = NodeInput(id="condition", type=bool)
     input = NodeInput(id="input", type=Any)
     do = NodeOutput(id="do", type=Any)
@@ -32,12 +35,33 @@ class WhileNode(Node):
             self.outputs["done"].value = input
 
 
+class WaitNode(Node):
+    node_id = "wait_node"
+    node_name = "Wait"
+    delay = NodeInput(
+        id="delay",
+        type=float,
+        required=True,
+        default=1.0,
+        does_trigger=False,
+        render_options={"step": "0.1"},
+        value_options={"min": 0.0},
+    )
+    input = NodeInput(id="input", type=Any)
+    output = NodeOutput(id="output", type=Any)
+
+    async def func(self, delay: float, input: Any) -> None:
+        await asyncio.sleep(delay)
+        self.outputs["output"].value = input
+
+
 class ForNode(Node):
     node_id = "for_node"
-    input = NodeInput(id="input", type=list)
+    node_name = "For"
+    input = NodeInput(id="input", type=List[Any])
     do = NodeOutput(id="do", type=Any)
     collector = NodeInput(id="collector", type=Any, does_trigger=False, required=False)
-    done = NodeOutput(id="done", type=Any)
+    done = NodeOutput(id="done", type=List[Any])
 
     async def func(self, input: list) -> None:
         results = []
@@ -49,3 +73,26 @@ class ForNode(Node):
             await triggerstack
             results.append(self.inputs["collector"].value)
         self.outputs["done"].value = results
+
+
+class CollectorNode(Node):
+    node_id = "collector_node"
+    node_name = "Collector"
+
+    reset = NodeInput(id="reset", type=Any, does_trigger=True, required=False)
+    input = NodeInput(id="input", type=Any)
+
+    output = NodeOutput(id="output", type=List[Any])
+    default_reset_inputs_on_trigger = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.collection = []
+
+    async def func(self, input: Any, reset: Any = NoValue) -> None:
+
+        if reset != NoValue:
+            self.collection = []
+
+        self.collection.append(input)
+        self.outputs["output"].value = self.collection
