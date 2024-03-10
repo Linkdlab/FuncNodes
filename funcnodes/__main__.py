8a6249bed82f2a4772e3fdab25dad86ec7342d5e
Julian Kimmig <julian.kimmig@gmx.net>
2024-03-10 06:44:46 +0100

    cli2main

--- funcnodes/__main__.py
+++ funcnodes/__main__.py
@@ -1,19 +1,27 @@
 from funcnodes.frontends.funcnodes_react import run_server
+import funcnodes as fn
 import argparse
 
 
 def main():
     parser = argparse.ArgumentParser(description="Funcnodes Cli.")
     parser.add_argument("task", help="The task to perform")
-    parser.add_argument("--port", help="Port to run the server on", default=8000)
+    parser.add_argument(
+        "--port",
+        help="Port to run the server on",
+        default=fn.config.CONFIG["frontend"]["port"],
+    )
+    parser.add_argument(
+        "--no-browser",
+        help="Open the browser after starting the server",
+        action="store_false",
+    )
     args = parser.parse_args()
 
     # Example of handling tasks
     if args.task == "runserver":
 
-        run_server(
-            port=args.port,
-        )
+        run_server(port=args.port, open_browser=args.no_browser)
 
     else:
         print(f"Unknown task: {args.task}")
