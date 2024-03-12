713f1898b870b17134ad18da0100456e045997d8
Julian Kimmig <julian.kimmig@gmx.net>
2024-03-12 14:59:15 +0100

    parses requirements and pyproject

--- funcnodes/lib/libfinder.py
+++ funcnodes/lib/libfinder.py
@@ -29,6 +29,20 @@ def find_shelf(src: str) -> Shelf | None:
         mod_name = os.path.basename(src)
         if mod_path not in sys.path:
             sys.path.append(mod_path)
+        if "requirements.txt" in os.listdir(mod_path):
+            # install pip requirements
+            os.system(f"pip install -r {mod_path}/requirements.txt")
+        if "pyproject.toml" in os.listdir(mod_path):
+            # install poetry requirements
+            # save current path
+            cwd = os.getcwd()
+            # cd into the module path
+            os.chdir(mod_path)
+            # install via poetry
+            os.system(f"poetry install -n --only main")
+            # cd back
+            os.chdir(cwd)
+
         try:
             mod = importlib.import_module(mod_name)
             return module_to_shelf(mod)
