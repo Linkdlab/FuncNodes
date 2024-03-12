9dda545af60c09c69b226ee23a84d826b499d28f
Julian Kimmig <julian.kimmig@gmx.net>
2024-03-12 12:50:39 +0100

    find shelves by string

--- funcnodes/lib/libfinder.py
+++ funcnodes/lib/libfinder.py
@@ -1,13 +1,36 @@
 import importlib
 from .lib import Shelf
 from .libparser import module_to_shelf
+import os
+import sys
 
 
-def find_shelf(module: str = None) -> Shelf | None:
+def find_shelf(src: str) -> Shelf | None:
     # check if identifier is a python module e.g. "funcnodes.lib"
-    if module is not None:
+
+    # try to use as module
+    try:
+        mod = importlib.import_module(src)
+        return module_to_shelf(mod)
+    except ModuleNotFoundError:
+        pass
+
+    # check if file path:
+    if src.startswith("file://"):
+        # unifiy path between windows and linux
+        src = src.replace("\\", "/")
+        src = src[7:].strip("/")
+        # check if file exists
+        if not os.path.exists(src):
+            raise FileNotFoundError(f"file {src} not found")
+
+        # get module path
+        mod_path = os.path.dirname(src)
+        mod_name = os.path.basename(src)
+        if mod_path not in sys.path:
+            sys.path.append(mod_path)
         try:
-            mod = importlib.import_module(module)
+            mod = importlib.import_module(mod_name)
             return module_to_shelf(mod)
         except ModuleNotFoundError:
             pass
