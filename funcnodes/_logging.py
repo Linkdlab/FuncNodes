ae1d37da14d032cf4b4d491892736e01120253b9
Julian Kimmig <julian.kimmig@gmx.net>
2024-03-22 13:18:15 +0100

    logging handler inher

--- funcnodes/_logging.py
+++ funcnodes/_logging.py
@@ -9,7 +9,7 @@ if not os.path.exists(LOGGINGDIR):
 
 FUNCNODES_LOGGER = logging.getLogger("funcnodes")
 
-FUNCNODES_LOGGER.setLevel("DEBUG")
+FUNCNODES_LOGGER.setLevel(logging.DEBUG)
 
 
 ch = logging.StreamHandler()
@@ -17,15 +17,31 @@ fh = RotatingFileHandler(
     os.path.join(LOGGINGDIR, "funcnodes.log"), maxBytes=1024 * 1024 * 5, backupCount=5
 )
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-fh.setFormatter(formatter)
+
 # Add the handler to the logger
+
+
+def _overwrite_add_handler(logger):
+    _old_add_handler = logger.addHandler
+
+    def _new_add_handler(hdlr):
+        hdlr.setFormatter(formatter)
+        if hdlr not in logger.handlers:
+            _old_add_handler(hdlr)
+
+    logger.addHandler = _new_add_handler
+
+
+_overwrite_add_handler(FUNCNODES_LOGGER)
+
 FUNCNODES_LOGGER.addHandler(ch)
 FUNCNODES_LOGGER.addHandler(fh)
 
 
-def get_logger(name):
+def get_logger(name, propagate=True):
     sublogger = FUNCNODES_LOGGER.getChild(name)
+    _overwrite_add_handler(sublogger)
+    sublogger.propagate = propagate
 
     # _init_logger(sublogger)
     return sublogger
