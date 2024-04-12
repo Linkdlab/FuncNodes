c04256a1494022d3507ed3cf823bfb1aee402877
Julian Kimmig <julian.kimmig@gmx.net>
2024-04-12 13:08:14 +0200

    files nodes

--- funcnodes/basic_nodes/__init__.py
+++ funcnodes/basic_nodes/__init__.py
@@ -1,11 +1,11 @@
 from funcnodes import Shelf
-from .frontend import NODE_SHELF as frontend_shelf
 from .logic import NODE_SHELF as logic_shelf
 from .math import NODE_SHELF as math_shelf
+from .files import NODE_SHELF as files_shelf
 
 NODE_SHELF = Shelf(
     nodes=[],
-    subshelves=[math_shelf, logic_shelf, frontend_shelf],
+    subshelves=[math_shelf, logic_shelf, files_shelf],
     name="basics",
     description="basic functionalities",
 )
