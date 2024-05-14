1a8fd62bcb0a3de0b4373a876d9a4227a6f161ef
Julian Kimmig <julian.kimmig@gmx.net>
2024-05-14 09:14:38 +0200

    files removed from basic nodes due to own package

--- funcnodes/basic_nodes/__init__.py
+++ funcnodes/basic_nodes/__init__.py
@@ -1,11 +1,13 @@
 from funcnodes import Shelf
 from .logic import NODE_SHELF as logic_shelf
 from .math import NODE_SHELF as math_shelf
-from .files import NODE_SHELF as files_shelf
 
 NODE_SHELF = Shelf(
     nodes=[],
-    subshelves=[math_shelf, logic_shelf, files_shelf],
+    subshelves=[
+        math_shelf,
+        logic_shelf,
+    ],
     name="basics",
     description="basic functionalities",
 )
