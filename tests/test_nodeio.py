c2aff5803e8ecb9c7c19c42fe816521d5bcba003
JulianKimmig <julian.kimmig@gmx.net>
2024-09-23 15:36:10 +0200

    update test

--- tests/test_nodeio.py
+++ tests/test_nodeio.py
@@ -42,6 +42,7 @@ class TestNodeIO(unittest.TestCase):
                 "full_id": None,
                 "default": "<NoValue>",
                 "required": True,
+                "hidden": False,
             },
         )
 
@@ -65,6 +66,7 @@ class TestNodeIO(unittest.TestCase):
                 "full_id": None,
                 "default": NoValue,
                 "required": True,
+                "hidden": False,
             },
         )
 
