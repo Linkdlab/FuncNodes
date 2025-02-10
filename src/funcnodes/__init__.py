c57e1ea969e21e4058c42ba3a37d8c30e0d56c06
JulianKimmig <julian.kimmig@gmx.net>
2025-02-10 11:09:51 +0100

    refactor: remove unused files and update imports for funcnodes_worker integration

--- src/funcnodes/__init__.py
+++ src/funcnodes/__init__.py
@@ -1,10 +1,9 @@
 from funcnodes_core import *  # noqa: F401, F403 # type: ignore
 from funcnodes_core import __all__ as core_all  # Explicit import
+from funcnodes_worker import *  # noqa: F401, F403 # type: ignore
+from funcnodes_core import __all__ as worker_all  # Explicit import
 
 from .worker import (
-    FuncNodesExternalWorker,
-    RemoteWorker,
-    WSWorker,
     WorkerManager,
     assert_worker_manager_running,
 )
@@ -14,14 +13,11 @@ from .patches import apply_patches
 apply_patches()
 
 __all__ = [
-    "FuncNodesExternalWorker",
-    "RemoteWorker",
-    "WSWorker",
     "WorkerManager",
     "assert_worker_manager_running",
 ]
 
-__all__ += core_all
+__all__ += worker_all + core_all
 
 
 __version__ = "0.5.33"
